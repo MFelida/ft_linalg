b3f881c36be84843fe3816e60a6e7665beb5168a
Mike Felida <mike.felida@gmail.com>
2024-10-06 01:24:43 +0200

    Added matrix print source to Makefile

--- src/mat_to_str.c
+++ src/mat_to_str.c
@@ -6,7 +6,7 @@
 /*   By: mfelida <mfelida@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 00:18:01 by mfelida           #+#    #+#             */
-/*   Updated: 2024/10/06 00:26:53 by mfelida          ###   ########.fr       */
+/*   Updated: 2024/10/06 01:03:24 by mfelida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,63 @@ char	*mat2_to_str(t_mat2 m, int n_dec)
 	char	*temp;
 	int		i;
 	
-	prev = NULL;
 	res = ft_strdup("");
+	prev = res;
 	i = 0;
 	while (i < 2)
 	{
 		temp = vec2_to_str(mat2_row_to_vec(m, i++), n_dec);
 		res = ft_strjoin(res, temp);
 		free(temp);
-		if (prev)
-			free(prev);
+		free(prev);
+		prev = res;
+		res = ft_strjoin(res, "\n");
+		free(prev);
+		prev = res;
+	}
+	return (res);
+}
+
+char	*mat3_to_str(t_mat3 m, int n_dec)
+{
+	char	*res;
+	char	*prev;
+	char	*temp;
+	int		i;
+	
+	res = ft_strdup("");
+	prev = res;
+	i = 0;
+	while (i < 3)
+	{
+		temp = vec3_to_str(mat3_row_to_vec(m, i++), n_dec);
+		res = ft_strjoin(res, temp);
+		free(temp);
+		free(prev);
+		prev = res;
+		res = ft_strjoin(res, "\n");
+		free(prev);
+		prev = res;
+	}
+	return (res);
+}
+
+char	*mat4_to_str(t_mat4 m, int n_dec)
+{
+	char	*res;
+	char	*prev;
+	char	*temp;
+	int		i;
+	
+	res = ft_strdup("");
+	prev = res;
+	i = 0;
+	while (i < 4)
+	{
+		temp = vec4_to_str(mat4_row_to_vec(m, i++), n_dec);
+		res = ft_strjoin(res, temp);
+		free(temp);
+		free(prev);
 		prev = res;
 		res = ft_strjoin(res, "\n");
 		free(prev);
