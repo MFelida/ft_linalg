be44fae92a3c364bb7326d09dfde609741981a0e
Mike Felida <mike.felida@gmail.com>
2024-10-10 23:16:19 +0200

    Made general vec_to_str local

--- src/vector_utils.c
+++ src/vector_utils.c
@@ -6,7 +6,7 @@
 /*   By: mfelida <mfelida@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 21:08:17 by mfelida           #+#    #+#             */
-/*   Updated: 2024/10/04 12:57:25 by mfelida          ###   ########.fr       */
+/*   Updated: 2024/10/10 23:15:44 by mfelida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "libft.h"
 #include <stdlib.h>
 
-char	*vecx_to_str(size_t n, size_t n_dec, float elems[])
+static char	*_vecx_to_str(size_t n, size_t n_dec, float elems[])
 {
 	char	*temp;
 	char	*res;
@@ -53,16 +53,16 @@ char	*vecx_to_str(size_t n, size_t n_dec, float elems[])
 
 char *vec2_to_str(t_vec2 v, size_t n_dec)
 {
-	return (vecx_to_str(2, n_dec, v.elements));
+	return (_vecx_to_str(2, n_dec, v.elements));
 }
 
 char *vec3_to_str(t_vec3 v, size_t n_dec)
 {
-	return (vecx_to_str(3, n_dec, v.elements));
+	return (_vecx_to_str(3, n_dec, v.elements));
 }
 
 char *vec4_to_str(t_vec4 v, size_t n_dec)
 {
-	return (vecx_to_str(4, n_dec, v.elements));
+	return (_vecx_to_str(4, n_dec, v.elements));
 }
 
